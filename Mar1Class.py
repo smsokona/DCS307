629ca8427da8b016e4c6a265fb89bc8decb2718e
smsokona <smangane@bates.edu>
2023-03-01 20:06:11 +0000

    committing changes, moving to R

---
+++ Mar1Class.py
@@ -0,0 +1,14 @@
+import simulus
+
+
+# Example in class
+
+sim = simulus.simulator()
+sim.sched(arrival, until =)
+
+def printMessage() -> None:
+  print(f"Hello world at time")
+  
+  
+  
+def arrival() _> 
